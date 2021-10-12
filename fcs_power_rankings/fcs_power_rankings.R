305c73ad12a0f5521030256a5fdaf9fb121641db
Andrew Block <andrewblock87@gmail.com>
2021-10-12 15:27:59 -0500

    Update fcs_power_rankings.R
    added outcomes column

--- fcs_power_rankings/fcs_power_rankings.R
+++ fcs_power_rankings/fcs_power_rankings.R
@@ -1,11 +1,6 @@
-# Clear space
-rm(list=ls())
-gc()
-
 library(tidyverse)
 library(gtExtras)
 library(gt)
-
 
 stack_word <- function(word_1, word_2){
   glue::glue(
@@ -14,25 +9,29 @@ stack_word <- function(word_1, word_2){
   )
 }
 
-# Load fcs data
-fcs <- readr::read_csv('https://raw.githubusercontent.com/andrew-block/tidy-vizzies/main/fcs_power_rankings/data/fcs_rankings_week_7.csv')
+fcs_rank <- readr::read_csv('https://raw.githubusercontent.com/andrew-block/tidy-vizzies/main/fcs_power_rankings/data/fcs_rankings_week_7.csv')
+fcs_wins <- readr::read_csv('https://raw.githubusercontent.com/andrew-block/tidy-vizzies/main/fcs_power_rankings/data/fcs_win_losses_week_7.csv')
+
+
+fcs <- fcs_wins %>% 
+  group_by(Team_Name) %>% 
+  summarise(
+    Wins = length(Win[Win==1]),
+    Losses = length(Win[Win==0]),
+    outcomes = list(Win), .groups = "drop") %>% 
+  left_join(fcs_rank, by = c("Team_Name" = "team")) %>% 
+  select(Team_Name, logo, conference, record, macro_ranking, change, dr_index, cfp_index, vs_index, sos, outcomes) %>%
+  arrange(macro_ranking)
 
 fcs_t <- fcs %>%
-  select(team,logo,conference,record,macro_ranking,change,dr_index,cfp_index,vs_index,sos) %>%
   mutate(
-    combo = stack_word(team, conference),
+    combo = stack_word(Team_Name, conference),
     combo = map(combo, gt::html)
   ) %>%
-  select(combo, logo, record, macro_ranking, change, dr_index, cfp_index, vs_index, sos) %>%
-  # Make table
+  select(combo, logo, record, outcomes, macro_ranking, change, dr_index, cfp_index, vs_index, sos) %>%
   gt() %>%
-
-  # Apply 'New York Times' theme
   gtExtras::gt_theme_538() %>%
-
   gtExtras::gt_img_rows(columns = logo, height = 20) %>%
-
-  # Add title and subtitle
   tab_header(
     title = "FCS Meta Power Rankings",
     # Use markdown syntax with md()
@@ -43,36 +42,39 @@ fcs_t <- fcs %>%
     locations = cells_column_labels(
       columns = sos)
   ) %>%
+  tab_footnote(
+    footnote = "Meta Ranking",
+    locations = cells_column_labels(
+      columns = macro_ranking)
+  ) %>%
   cols_align(
     align = "left",
     columns = c(combo)
   ) %>%
   tab_options(
-    data_row.padding = px(5),
     heading.title.font.size = px(40),
     heading.title.font.weight = "bolder",
     heading.subtitle.font.size =  px(20),
     footnotes.font.size = px(12)
   ) %>%
   cols_label(
-    logo = "  ",
+    logo = "",
     combo = "Team",
     dr_index = "DR",
     cfp_index = "CFP",
     vs_index = "VS",
-    macro_ranking = "Rank"
+    macro_ranking = "MR",
+    change = "+/-"
   ) %>%
   text_transform(
     locations = cells_body(columns = change),
     fn = function(x){
-
       change <- as.integer(x)
-
       choose_logo <-function(x){
         if (x == 0){
           gt::html(fontawesome::fa("equals", fill = "#696969"))
         } else if (x > 0){
-          gt::html(glue::glue("<span style='color:#191970;font-face:bold;font-size:10px;'>{x}</span>"), fontawesome::fa("arrow-up", fill = "#1134A6"))
+          gt::html(glue::glue("<span style='color:#191970;text-indent:10px;font-face:bold;font-size:10px;'>{x}</span>"), fontawesome::fa("arrow-up", fill = "#1134A6"))
         } else if (x < 0) {
           gt::html(glue::glue("<span style='color:#DA2A2A;font-face:bold;font-size:10px;'>{x}</span>"), fontawesome::fa("arrow-down", fill = "#DA2A2A"))
         }
@@ -117,10 +119,27 @@ fcs_t <- fcs %>%
     columns = vars(dr_index),
     decimals = 2
   ) %>%
+  cols_width(
+    vars(combo) ~ px(35),
+    vars(logo) ~ px(6),
+    vars(record) ~ px(15),
+    vars(macro_ranking) ~ px(9),
+    vars(change) ~ px(9),
+    vars(cfp_index) ~ px(15),
+    vars(dr_index) ~ px(16),
+    vars(vs_index) ~ px(13),    
+    vars(sos) ~ px(13),
+    vars(outcomes) ~ px(20)  
+  ) %>% 
+  tab_options(
+    table.width = px(700),
+    data_row.padding = px(4)
+  ) %>%
   cols_align(
     align = "center",
-    columns = c(record, macro_ranking, change, dr_index, cfp_index, vs_index, sos)
+    columns = c(record, macro_ranking, dr_index, cfp_index, vs_index, sos)
   ) %>%
+  gt_plt_winloss(outcomes, max_wins = 6) %>% 
   data_color(
     columns = sos,
     colors = scales::col_numeric(
@@ -129,5 +148,6 @@ fcs_t <- fcs %>%
         "#ccebc5", "#e0f3db", "#f7fcf0"),
       domain = NULL
     )
-    )
-  fcs_t
+  )
+
+fcs_t
