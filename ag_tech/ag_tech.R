545f76268e0e1d51e35cc1650da942223a5e606f
andrewblock <ablock@bushelpowered.com>
2021-10-22 15:25:51 -0500

    updated ag tech data callable

--- ag_tech/ag_tech.R
+++ ag_tech/ag_tech.R
@@ -6,6 +6,6 @@ library(ggridges)
 library(ggthemes)
 library(ggtext)
 
-rm(list = ls())
+ag <- readr::read_csv('https://raw.githubusercontent.com/andrew-block/tidy-vizzies/main/ag_tech/data/ag_tech_2021.csv')
 
-ag <- readr::read_csv('https://raw.githubusercontent.com/andrew-block/tidy-vizzies/main/ag_tech/data/ag_tech.csv')
+ag
